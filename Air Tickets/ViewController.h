84f2a8d36673185cd66f6af10700aac80c68f575
Nelmeris <kufaevartem@icloud.com>
2019-03-17 19:11:25 +0300

    Initial Commit

---
+++ Air Tickets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Air Tickets
+//
+//  Created by Artem Kufaev on 17/03/2019.
+//  Copyright © 2019 Artem Kufaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Air Tickets/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Air Tickets
+//
+//  Created by Artem Kufaev on 17/03/2019.
+//  Copyright © 2019 Artem Kufaev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
